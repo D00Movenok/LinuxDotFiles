24a30323be71da8201d9b4186441065a296e1700
D00Movenok <egor.gennadjev@yandex.ru>
2025-07-18 22:42:14 +0300

    add tmux theme

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -9,21 +9,21 @@ rm -rf ${HOME}/.config/alacritty
 ln -sv ${script_dir}/ ${HOME}/.config/alacritty
 
 # add theme
-git clone https://github.com/alacritty/alacritty-theme themes
+git clone https://github.com/alacritty/alacritty-theme ${script_dir}/themes
 
 # update default terminal for systems that uses x-terminal-emulator
 sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 
 # update default terminal for systems that uses xdg-terminal-exec
 # (e.g. ubuntu 25.04)
-if [ -f /usr/share/applications/Alacritty.desktop ]; then
-    alacritty_desktop="Alacritty.desktop"
-else
+alacritty_desktop="Alacritty.desktop"
+if [ ! -f /usr/share/applications/$alacritty_desktop ]; then
     echo "Alacritty desktop file not found in /usr/share/applications/"
     exit 1
 fi
 find "$HOME" -type f -name '*xdg-terminals.list' | while read -r file; do
     if ! grep -q "$alacritty_desktop" "$file"; then
+        # prepend the alacritty desktop to the file
         sed -i "1i$alacritty_desktop" "$file"
     fi
 done
--- tmux/install.sh
+++ tmux/install.sh
@@ -2,6 +2,14 @@
 
 script_dir=$(dirname $(readlink -f "$0"))
 
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ${script_dir}/tmux/plugins/tpm
+rm -rf ${HOME}/.tmux
+ln -sv ${script_dir}/tmux ${HOME}/.tmux
+
 # install tmux config
 rm -rf ${HOME}/.tmux.conf
 ln -sv ${script_dir}/tmux.conf ${HOME}/.tmux.conf
+
+# install plugins
+${script_dir}/tmux/plugins/tpm/bin/install_plugins
