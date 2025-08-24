2704afad0006eb59430a68d5c748a698c4516c31
D00Movenok <egor.gennadjev@yandex.ru>
2025-08-24 22:40:19 +0300

    add php to asdf

--- 00-preinstall/other-langs.sh
+++ 00-preinstall/other-langs.sh
@@ -6,3 +6,6 @@ sudo apt install -y nodejs
 # rust
 sudo apt install -y rustup
 rustup default stable
+
+# php
+sudo apt install -y php
--- asdf/install.sh
+++ asdf/install.sh
@@ -7,6 +7,13 @@ go install github.com/asdf-vm/asdf/cmd/asdf@latest
 # managed languages
 asdf plugin add nodejs
 asdf plugin add python
+asdf plugin add php
+
+# TODO: replace with just php when
+# https://github.com/asdf-community/asdf-php/pull/172
+# will be merged
+asdf plugin add php https://github.com/pmysiak/asdf-php
+asdf plugin update php patch-1
 
 # python dependencies
 # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
